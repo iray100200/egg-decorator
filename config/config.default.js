04e80061d22bbd8e5a6a5ca8079d187142180706
Raymond <iray100200@hotmail.com>
2019-03-31 17:29:55 +0800

    update

--- config/config.default.js
+++ config/config.default.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 exports.decorator = {
   app: true,
--- test/app.js
+++
@@ -1,8 +0,0 @@
-const babelConfig = require('../babel.config');
-
-require('@babel/register')(babelConfig);
-
-module.exports = app => {
-  const { logger } = app;
-  logger.info('[master]', 'register egg decorator');
-};
